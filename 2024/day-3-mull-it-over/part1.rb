f1d36910a51e0f30daefa5e384c667aceca07454
Erik Yuzwa <erikyuzwa@gmail.com>
2024-12-03 08:53:27 -0700

    day 4 part 1 done

---
+++ 2024/day-3-mull-it-over/part1.rb
@@ -0,0 +1,25 @@
+# part 1
+
+inputfile = "input.txt"
+accumulator = 0
+match_count = 0
+
+content = File.read(inputfile)
+
+operation_re = /mul\(\d+,\d+\)/
+
+matches = content.scan(operation_re)
+matches.each do |match|
+  #puts "#{match}"
+
+  operand_matches = match.scan(/\d+/)
+  # puts "#{operand_matches}"
+  result = operand_matches[0].to_i * operand_matches[1].to_i
+
+  accumulator += result
+end
+
+puts "processed #{matches.count} matches."
+puts "computed result: #{accumulator}"
+
+# computed result = 187194524 which is the correct answer given the data
