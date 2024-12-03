93c8369744a6ec05f0c2d0aa3950cfcf51620abe
Erik Yuzwa <erikyuzwa@gmail.com>
2024-12-03 14:48:38 -0700

    updated day3 part2!

--- 2024/day-3-mull-it-over/part2.rb
+++ 2024/day-3-mull-it-over/part2.rb
@@ -1,25 +1,95 @@
 # part 2
- 
-inputfile = "input2.txt"
+
+def is_digit?(s)
+  code = s.ord
+  # 48 is ASCII code of 0
+  # 57 is ASCII code of 9
+  48 <= code && code <= 57
+end
+
+inputfile = "input.txt"
 accumulator = 0
 match_count = 0
+mul_enabled = true # variable to track do() or don't()
 
 content = File.read(inputfile)
 
-operation_re = /mul\(\d+,\d+\)/
+# brute force character by character
+index = 0
+puts "mul_enabled = true"
+while (index != content.length)
+  char = content[index]
+  puts "#{content[index]}"
 
-matches = content.scan(operation_re)
-matches.each do |match|
-  # puts "#{match}"
+  if char.downcase == 'd' then
+    if content[index + 1] == 'o' then
+      if content[index + 2] == '(' then
+        if content[index + 3] == ')' then
+          index += 3
+          mul_enabled = true
+          puts "mul_enabled = true"
+        end
+      end
+    end
+  end
 
-  operand_matches = match.scan(/\d+/)
-  # puts "#{operand_matches}"
-  result = operand_matches[0].to_i * operand_matches[1].to_i
+  if char.downcase == 'd' then
+    if content[index + 1] == 'o' then
+      if content[index + 2] == 'n' then
+        if content[index + 3] == '\'' then
+          if content[index + 4] == 't' then
+            if content[index + 5] == '(' then
+              if content[index + 6] == ')' then
+                index += 6
+                mul_enabled = false
+                puts "mul_enabled = false"
+              end
+            end
+          end
+        end
+      end
+    end
+  end
 
-  accumulator += result
+  if char.downcase == 'm' then
+    if content[index + 1].downcase == 'u' then
+      if content[index + 2].downcase == 'l' then
+        if content[index + 3] == '(' then
+          index += 4
+          op1_s = ""
+          op2_s = ""
+
+          while (content[index] != ',' && is_digit?(content[index]))
+            op1_s += content[index]
+            index += 1
+          end
+
+          # handle the ','
+          index += 1
+
+          while (content[index] != ')' && is_digit?(content[index]))
+            op2_s += content[index]
+            index += 1
+          end
+
+          #require 'byebug'; byebug
+          if content[index] == ')' then
+            puts "mul: (#{op1_s}, #{op2_s})"
+  
+            match_count += 1
+            accumulator += op1_s.to_i * op2_s.to_i if mul_enabled
+          end
+        end
+      end
+    end
+  end
+          
+  index += 1
 end
 
-puts "processed #{matches.count} matches."
+puts "processed #{match_count} matches."
 puts "computed result: #{accumulator}"
 
-#
+# 717 matches
+# computed result: 127092535
+
